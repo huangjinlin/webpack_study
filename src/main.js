eb653b99674021d4b59eed8837349c9fb4e3bff0
huangjinlin <416921332@qq.com>
2020-05-08 16:25:24 +0800

    完成普通文件的loader

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import $ from "jquery";
+import './css/1.css';
+import './css/1.less';
+import './css/1.scss';
 $(function(){
     $("li:odd").css("background","red");
     $("li:even").css("background","pink");
 })
+
+class Person {
+    static info = 'aaa'
+}
+console.log(Person.info)
