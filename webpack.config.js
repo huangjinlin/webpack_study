7227ff9bdd822d5e2a6418982b7cef4acaa89a8b
huangjinlin <416921332@qq.com>
2020-05-08 14:46:18 +0800

    完成html-webpack-plugin的使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path')
+//导入包
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+//创建对象
+const htmlPlugin = new HtmlWebpackPlugin({
+    //设置生成预览页面的模板文件
+    template: "./src/index.html",
+    //设置生成的预览页面名称
+    filename: "index.html"
+})
 module.exports = {
-    mode:"development",//可以设置为development(开发模式)，production(发布模式),
+    mode: "development",//可以设置为development(开发模式)，production(发布模式),
     entry: path.join(__dirname, './src/main.js'),//__dirname是webpack.config.js所在的路径
     output: {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
-    }
+    },
+    plugins:[ htmlPlugin ]
 }
